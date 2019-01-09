a5d9f5abc7b3abafa7148a153737592eb83fdfd8
jbabu63 <josephbabu32@gmail.com>
2019-01-09 11:45:04 +0530

    Firstcommit for Maven

---
+++ src/test/java/com/wipro/base/TestBase.java
@@ -0,0 +1,24 @@
+package com.wipro.base;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+public class TestBase {
+	
+	public static WebDriver driver;
+	
+	@BeforeSuite
+	public void setUp()
+	{
+		
+	}
+	
+	@AfterSuite
+	public void tearDown()
+	{
+		
+	}
+
+}
+
